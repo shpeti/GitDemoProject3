96a7de80cf658a99683bd452b8f8fd2b264433a6
ADMIN <ADMIN@HP>
2021-05-05 23:24:09 -0700

    Commiting Test3 to new Test4

--- src/Test4.java
+++ src/Test4.java
@@ -1,11 +1,12 @@
 
-public class Test3 {
+public class Test4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello Shridhar");
-		
+		System.out.println("Hi Sp");
+		System.out.println("First Try");
 	}
 
 }
